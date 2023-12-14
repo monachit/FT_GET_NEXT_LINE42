b29cfcf9c278a3b2c0f86af8e7f8dceb53857bf1
mo-nachit <111872760+monachit@users.noreply.github.com>
2023-12-14 19:02:38 +0100

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,16 +6,21 @@
 /*   By: mnachit <mnachit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 05:25:37 by mnachit           #+#    #+#             */
-/*   Updated: 2023/12/13 14:37:38 by mnachit          ###   ########.fr       */
+/*   Updated: 2023/12/14 18:51:09 by mnachit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-static void	ft_read(int fd, char *buf, char **str)
+static void	ft_free(char **str)
+{
+	free(*str);
+	*str = NULL;
+}
+
+static void	ft_read(int fd, char *buf, char **str, ssize_t i)
 {
 	char	*tmp;
-	ssize_t	i;
 
 	if (!str[fd] || !ft_strchr(str[fd], '\n'))
 	{
@@ -36,18 +41,14 @@ static void	ft_read(int fd, char *buf, char **str)
 			i = read(fd, buf, BUFFER_SIZE);
 		}
 	}
+	if (i == -1)
+		ft_free(&str[fd]);
 	free(buf);
-}
-
-static void	ft_free(char **str)
-{
-	free(*str);
-	*str = NULL;
 }
 
 static char	*ft_finish(char **str, int fd)
 {
-	char	*rja3;
+	char	*ret;
 	char	*tmp;
 	int		i;
 	int		j;
@@ -56,36 +57,35 @@ static char	*ft_finish(char **str, int fd)
 		return (NULL);
 	if (!ft_strchr(str[fd], '\n'))
 	{
-		rja3 = ft_substr(str[fd], 0, ft_strlen(str[fd]));
+		ret = ft_substr(str[fd], 0, ft_strlen(str[fd]));
 		ft_free(&str[fd]);
-		return (rja3);
+		return (ret);
 	}
 	i = ft_strlen(str[fd]);
 	j = ft_strlen(ft_strchr(str[fd], '\n'));
-	rja3 = ft_substr(str[fd], 0, i - j + 1);
+	ret = ft_substr(str[fd], 0, i - j + 1);
 	tmp = str[fd];
 	str[fd] = ft_substr(ft_strchr(str[fd], '\n'), 1, j - 1);
+	if (!str[fd])
+		return (NULL);
 	free(tmp);
 	if (*str[fd] == '\0')
 		ft_free(&str[fd]);
-	return (rja3);
+	return (ret);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*str[1024];
+	static char	*str[OPEN_MAX];
 	char		*buf;
+	ssize_t		i;
 
-	if (BUFFER_SIZE < 1 || BUFFER_SIZE > 4294967295)
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE < 1 || BUFFER_SIZE > 2147483647)
 		return (NULL);
-	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	buf = malloc(((size_t)BUFFER_SIZE + 1) * sizeof(char));
 	if (!buf)
 		return (NULL);
-	if (fd == -1 || read(fd, buf, 0) == -1)
-	{
-		free(buf);
-		return (NULL);
-	}
-	ft_read(fd, buf, str);
+	ft_read(fd, buf, str, i);
 	return (ft_finish(str, fd));
 }
